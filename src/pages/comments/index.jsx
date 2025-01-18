f44e4ecf2279e2c082b71e60f8fb7ce3e1fd7154
Taner Acar <151092957+taneracar@users.noreply.github.com>
2025-01-18 23:58:40 +0300

    using SWR library for custom hooks

--- src/pages/comments/index.jsx
+++ src/pages/comments/index.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-import useFetch from "../../hooks/use-fetch";
+import useSWR from "swr";
+//import useFetch from "../../hooks/use-fetch";
 
 const CommentsList = () => {
-  const { data, loading, error } = useFetch("https://dummyjson.com/comments");
-  if (loading) return <h1>Fetching Comments! Please wait</h1>;
-  console.log(data?.comments);
+  /* const { data, loading, error } = useFetch("https://dummyjson.com/comments");
+  if (loading) return <h1>Fetching Comments! Please wait</h1>; */
+  /*  console.log(data?.comments); */
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data, error, isLoading } = useSWR(
+    "https://dummyjson.com/comments",
+    fetcher
+  );
+  console.log(data, error, isLoading);
   return (
     <div>
       <ul>
@@ -16,6 +23,15 @@ const CommentsList = () => {
               </div>
             ))
           : null}
+
+        {/*   {data?.comments?.length > 0
+          ? data.comments.map((commentItem, key) => (
+              <div key={key}>
+                <h4>{commentItem?.body}</h4>
+                <h5>{commentItem?.user?.fullName}</h5>
+              </div>
+            ))
+          : null} */}
       </ul>
     </div>
   );
